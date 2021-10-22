ff89209960396086a2173cce7f4ac40623719608
Marty <maruri2424@gmail.com>
2021-10-22 01:27:50 -0700

    removed loop in game()

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function computerPlay() {
   let roll = Math.floor(Math.random() * 3);
   switch (roll) {
     case 0: 
-      return 'Rock';
+      return 'rock';
       break;
     case 1: 
-      return 'Paper';
+      return 'paper';
       break;
     case 2: 
-      return 'Scissors';
+      return 'scissors';
       break;
     default:
       return '';
@@ -16,49 +16,86 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let draw = 'Draw!';
-  let pWin = 'Player wins!';
-  let cWin = 'Computer wins!';
+  playerSelection = playerSelection.toLowerCase();
+  let draw = {win:'draw', message:'Draw!'};
+  let pWin = {win:'player', message:'Player wins!'};
+  let cWin = {win:'computer', message:'Computer wins!'};
+  let cError = {win:'error', message:'Computer error'};
+  let pError = {win:'error', message:'Player error'};
 
-  if (playerSelection === 'Rock') {
+  if (playerSelection === 'rock') {
     switch (computerSelection) {
-      case 'Rock':
+      case 'rock':
         return draw;
         break;
-      case 'Paper':
+      case 'paper':
         return cWin;
         break;
-      case 'Scissors':
+      case 'scissors':
         return pWin;
         break;
+      default:
+        return cError;
     }
-  } else if (playerSelection === 'Paper') {
+  } else if (playerSelection === 'paper') {
     switch (computerSelection) {
-      case 'Rock':
+      case 'rock':
         return pWin;
         break;
-      case 'Paper':
+      case 'paper':
         return draw;
         break;
-      case 'Scissors':
+      case 'scissors':
         return cWin;
         break;
+      default:
+        return cError;
     }
-  } else if (playerSelection === 'Scissors') {
+  } else if (playerSelection === 'scissors') {
     switch (computerSelection) {
-      case 'Rock':
+      case 'rock':
         return cWin;
         break;
-      case 'Paper':
+      case 'paper':
         return pWin;
         break;
-      case 'Scissors':
+      case 'scissors':
         return draw;
         break;
       default:
-        return 'Invalid '
+        return cError;
     }
   } else {
-
+    return pError;
   }
 }
+
+function game() {
+  let pScore = 0;
+  let cScore = 0;
+  let draw = 0;
+  let playerSelection = '';
+  while (!playerSelection) {
+    playerSelection = prompt('Choose Rock, paper, or scissors', '');
+  }
+  let computerSelection = computerPlay();
+  let result = playRound(playerSelection, computerSelection);
+  if (result.win === 'player') {
+    pScore++;
+  } else if (result.win === 'computer') {
+    cScore++;
+  } else if (result.win === 'draw') {
+    draw++;
+  }
+  console.log(result.message);
+  /*   
+  let endMsg = 
+    `Player score: ${pScore}
+    Computer score: ${cScore}
+    Draw: ${draw}
+    ${(pScore > cScore) ? 'Player wins!' : (cScore > pScore) ?
+        'Computer wins!' : 'Draw!'}`;
+  console.log(endMsg);
+  */
+}
+
